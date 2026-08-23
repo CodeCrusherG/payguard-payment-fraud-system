327b6b2ef07ff8af78e6b25305ee3b5e7e457c63
CodeCrusherG <b24126@students.iitmandi.ac.in>
2026-08-24 01:42:16 +0530

    Update kafka_producer.py

--- fraud-service/app/kafka_producer.py
+++ fraud-service/app/kafka_producer.py
@@ -7,6 +7,10 @@ producer = KafkaProducer(
     "KAFKA_BOOTSTRAP_SERVERS",
     "localhost:9092"
 ),
+    security_protocol="SASL_SSL",
+    sasl_mechanism="SCRAM-SHA-256",
+    sasl_plain_username=os.getenv("KAFKA_SASL_USERNAME"),
+    sasl_plain_password=os.getenv("KAFKA_SASL_PASSWORD"),
     value_serializer=lambda value:
         json.dumps(value).encode("utf-8")
 )
